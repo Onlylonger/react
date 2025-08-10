84129f08efebb0ed71e08ef2be4445669f65e2b3
barry <onlylonger@outlook.com>
2025-08-10 09:18:41 +0800

    U

---
+++ sl-ui/playground/src/App.tsx
@@ -0,0 +1,10 @@
+import { MyButton } from '../../dist/index'
+import '../../dist/global.css'
+
+export function App() {
+  return (
+    <>
+      <MyButton type="primary" />
+    </>
+  )
+}
---
+++ sl-ui/src/index.ts
@@ -0,0 +1,2 @@
+import './global.css'
+export { MyButton } from './MyButton'
